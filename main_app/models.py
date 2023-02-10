6b7ed753d973b9f40eb6e89282719db2fdec328e
scaev <kenkaraa@gmail.com>
2023-02-10 17:15:55 -0500

    Adds model structre

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=30)
+    email = models.EmailField()
+    password = models.CharField(max_length=256)
+    date_created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.username
+
+class Auction(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    starting_price = models.DecimalField(max_digits=10, decimal_places=2)
+    end_date = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
+
+class Bid(models.Model):
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date_created = models.DateTimeField(auto_now_add=True)
+    auction = models.ForeignKey(Auction, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.amount} on {self.auction}"
--- main_app/urls.py
+++ main_app/urls.py
@@ -1,7 +1,6 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('main_app.urls')),
+    path('', views.home, name='home'),
 ]
