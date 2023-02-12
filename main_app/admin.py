1edd6936a4342253eb3ef55eb13f6303e3731061
scaev <kenkaraa@gmail.com>
2023-02-12 14:57:42 -0500

    Adds functionality for detail page

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Auction
+from .models import Auction, User
 
 # Register your models here.
 admin.site.register(Auction)
+admin.site.register(User)
