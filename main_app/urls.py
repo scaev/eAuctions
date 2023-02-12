5a0e13b89f5227191dd1b1c5547098d46b8f66f5
scaev <kenkaraa@gmail.com>
2023-02-12 11:47:56 -0500

    Adds index, home views

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('cats/', views.auctions_index, name='index'),
 ]
