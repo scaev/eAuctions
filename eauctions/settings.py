f6c3709796ec31215c9e7a0ea8eb423a238a2d28
Kaan <kenkaraa@gmail.com>
2023-03-22 14:32:32 -0400

    Changes for deployment

--- eauctions/settings.py
+++ eauctions/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--65m0_v4lx*y_1sgnwtbdj0ohg3-k=5gjefg5s((80f^cwck85'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://eauctions.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
